9df306503ea0fde1218eec1ff2abaff183b115ba
mitchelldschwartz <sch10028@byui.edu>
2017-06-12 23:00:35 -0600

    Test for uploading java

---
+++ CIT360Project/src/cit360project/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+        String text = "Hello World!";
+	System.out.println (text);
+    }
+}
