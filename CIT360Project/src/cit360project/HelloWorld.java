28ed9cbef8605926e6b31bcfc2ff0ad73d16dab5
mitchelldschwartz <sch10028@byui.edu>
2017-11-20 15:30:10 -0700

    Back to just world

--- CIT360Project/src/cit360project/HelloWorld.java
+++ CIT360Project/src/cit360project/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-        String text = "Hello Worldssss!";
+        String text = "Hello World!";
 	System.out.println (text);
     }
 }
